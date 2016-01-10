16c99fa9476e47cd7e8680d4f90b47747b641bcc
Maxat Kulmanov <coolmaksat@gmail.com>
2016-01-10 23:15:07 +0300

    next level

--- gen_next_level_training.py
+++ gen_next_level_training.py
@@ -10,19 +10,59 @@ LAMBDA = 24
 DATA_ROOT = 'data/swiss/'
 
 go = get_gene_ontology()
-go_model = dict()
 
 
-def load_data(go_id):
-    data = list()
+def get_go_set(go_id):
+    go_set = set()
+    q = deque()
+    q.append(go_id)
+    while len(q) > 0:
+        g_id = q.popleft()
+        go_set.add(g_id)
+        for ch_id in go[g_id]['children']:
+            q.append(ch_id)
+    return go_set
+
+
+def get_subtree_set(go_id):
+    node = go[go_id]
+    if 'go_set' in node:
+        return node['go_set']
+    go_set = set()
+    go_set.add(go_id)
+    for ch_id in node['children']:
+        if ch_id not in go_set:
+            go_set |= get_subtree_set(ch_id)
+    node['go_set'] = go_set
+    return go_set
+
+
+def load_data_by_prot_id(go_id):
+    data = dict()
     with open(DATA_ROOT + 'level_2/data/' + go_id + '.txt') as f:
         for line in f:
             line = line.strip().split()
             prot_id = line[0]
-            paac = list()
-            for v in line[1:]:
-                paac.append(float(v))
-            data.append((prot_id, paac))
+            data[prot_id] = line[1:]
+    return data
+
+
+def load_training_data(go_id):
+    go_set = get_subtree_set(go_id)
+    data = list()
+    with open(DATA_ROOT + 'train.txt', 'r') as f:
+        for line in f:
+            line = line.strip().split('\t')
+            prot_id = line[0]
+            gos = line[2].split('; ')
+            ok = False
+            go_ids = list()
+            for go_id in gos:
+                if go_id in go_set:
+                    ok = True
+                    go_ids.append(go_id)
+            if ok:
+                data.append((prot_id, go_ids))
     return data
 
 
@@ -30,26 +70,36 @@ def main(*args, **kwargs):
     if len(args) != 2:
         raise Exception('Please provide function id')
     go_id = args[1]
-    data = load_data(go_id)
-    model = get_model(go_id)
-    paacs = list()
-    for prot_id, paac in data:
-        paacs.append(paac)
-    pred = model.predict_classes(
-        numpy.array(paacs, dtype="float32"),
-        batch_size=1,
-        verbose=1)
-    result = list()
-    for i in range(len(data)):
-        if pred[i] == 1:
-            result.append(data[i])
-
-    with open(DATA_ROOT + 'level_2/data/' + go_id + '.txt', 'w') as f:
-        for prot_id, paac in result:
-            f.write(prot_id)
-            for p in paac:
-                f.write(' ' + str(p))
-            f.write('\n')
+    paacs = load_data_by_prot_id(go_id)
+    data = load_training_data(go_id)
+    go_node = go[go_id]
+    go_set = get_subtree_set(go_id)
+    for ch_id in go_node['children']:
+        ch_set = get_subtree_set(ch_id)
+        positives = list()
+        negatives = list()
+        for prot_id, gos in data:
+            if prot_id not in paacs:
+                continue
+            pos = False
+            for go_id in gos:
+                if go_id in ch_set:
+                    pos = True
+                    break
+            if pos:
+                positives.append(prot_id)
+            else:
+                negatives.append(prot_id)
+        n = len(positives)
+        shuffle(positives)
+        shuffle(negatives)
+        negatives = negatives[:n]
+        with open(DATA_ROOT + 'level_2/' + go_id + '/' + ch_id + '.txt', 'w') as f:
+            for prot_id in negatives:
+                f.write(prot_id)
+                for p in paacs[prot_id]:
+                    f.write(' ' + str(p))
+                f.write('\n')
 
 
 if __name__ == '__main__':
