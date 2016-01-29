d8a42129a36dfd04655a33c0b36ce34dc1f41c8a
Maxat Kulmanov <coolmaksat@gmail.com>
2016-01-29 07:31:47 +0300

    fix

--- seq2props.py
+++ seq2props.py
@@ -92,7 +92,7 @@ def main(*args, **kwargs):
             for prop in props:
                 f.write(label)
                 for x in seq[:500]:
-                    f.write('\t' + x)
+                    f.write('\t' + prop[x])
                 for x in range(n, 500):
                     f.write(' 0.0')
                 f.write('\n')
